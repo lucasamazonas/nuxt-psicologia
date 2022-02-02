115e33faf40c7d6af91ec006bd251feb4e1222cf
lucasamazonas <lucassilvaamazonas@gmail.com>
2022-02-01 20:55:34 -0400

    $axios configurado

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <Tutorial />
+  <div>
+    Olá mundo!
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async asyncData ({ $axios }) {
+    await $axios.$get('http://localhost/pscologia/api-psicologia/public/sanctum/csrf-cookie')
+      .then(() => {
+        console.log('Proteção csrf ativada.')
+      })
+      .catch(() => {
+        console.log('Erro ao solicitar proteção csrf.')
+      })
+  }
 })
 </script>
